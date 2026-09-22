a5e6aadfeb74aa889ad539063063097b91ebf72e
MrCrazylion <31191460+MrCrazylion@users.noreply.github.com>
2026-09-22 08:56:23 +0300

    Verify the GNU base wave as an isolated build matrix

--- onx/importer.py
+++ onx/importer.py
@@ -79,6 +79,8 @@ def import_packages(policy_path, names, output, gentoo=None):
                 "source_dir": adapter.get("source_dir", name + "-{version}").replace("{version}", version),
                 "build_system": adapter["build_system"], "configure_args": adapter.get("configure_args", []),
                 "status": status, **{k: deps.get(k, []) for k in ("builddeps", "targetdeps", "checkdeps", "rundeps")}}
+        if "check_command" in adapter:
+            meta["check_command"] = adapter["check_command"]
         validate(meta)
         for key in ("builddeps", "targetdeps", "checkdeps", "rundeps"):
             for dep in meta[key]:
